fa3f20b4325bb4b515d2eea0dda2281a3b99fdf6
carlosbrando <carlosbrando@04ac611f-ab46-0410-9913-d8964a4b2d5a>
2008-02-22 21:21:11 +0000

    All changed... see: http://www.nomedojogo.com/559/
    git-svn-id: http://plugins.nomedojogo.com/custom_resource_name@3 04ac611f-ab46-0410-9913-d8964a4b2d5a

--- lib/custom_resource_name.rb
+++ lib/custom_resource_name.rb
@@ -1,44 +1,90 @@
 module ActionController
   module Resources
     class Resource
-      attr_reader :path_segment, :edit_as
+      attr_reader :path_segment, :actions_as, :resources_as
 
       def initialize(entities, options)
         @plural   ||= entities
         @singular ||= options[:singular] || plural.to_s.singularize
-        @path_segment = options.delete(:as) || @plural 
         
-        @new_as ||= options.delete(:new_as) || 'new'
-        @edit_as ||= options.delete(:edit_as) || 'edit'
-
+        @resources_as = options.delete(:resources_as) || {}
+        @actions_as = options.delete(:actions_as) || {}
+        
+        @path_segment = options.delete(:as) || @resources_as[entities] || @plural
+        
         @options = options
-
+        
         arrange_actions
         add_default_actions
         set_prefixes
       end
-
+      
       def path
         @path ||= "#{path_prefix}/#{path_segment}"
       end
-
-      def new_path
-        @new_path ||= "#{path}/#{@new_as}"
-      end
       
+      def new_path
+        action_new = @actions_as[:new] || 'new'
+        @new_path ||= "#{path}/#{action_new}"
+      end
     end
-  
+
+    def map_resource(entities, options = {}, &block)
+      options.merge!(:actions_as => @actions_as, :resources_as => @resources_as)
+      resource = Resource.new(entities, options)
+
+      with_options :controller => resource.controller do |map|
+        map_collection_actions(map, resource)
+        map_default_collection_actions(map, resource)
+        map_new_actions(map, resource)
+        map_member_actions(map, resource)
+
+        map_associations(resource, options)
+
+        if block_given?
+          with_options(:path_prefix => resource.nesting_path_prefix, :name_prefix => resource.nesting_name_prefix, :namespace => options[:namespace], &block)
+        end
+      end
+    end
+
+    def aliases(*args)
+      if args.size > 0
+        options = args.extract_options!
+        if args[0] == :resources
+          @resources_as = options || {}
+        elsif args[0] == :actions
+          @actions_as = options || {}
+        end
+      end
+    end
+
+    def map_collection_actions(map, resource)
+      resource.collection_methods.each do |method, actions|
+        actions.each do |action|
+          action_options = action_options_for(action, resource, method)
+
+          if resource.actions_as.has_key?(action)
+            map.named_route("#{action}_#{resource.name_prefix}#{resource.plural}", "#{resource.path}#{resource.action_separator}#{resource.actions_as[action]}", action_options)
+            map.named_route("formatted_#{action}_#{resource.name_prefix}#{resource.plural}", "#{resource.path}#{resource.action_separator}#{resource.actions_as[action]}.:format", action_options)
+          else
+            map.named_route("#{action}_#{resource.name_prefix}#{resource.plural}", "#{resource.path}#{resource.action_separator}#{action}", action_options)
+            map.named_route("formatted_#{action}_#{resource.name_prefix}#{resource.plural}", "#{resource.path}#{resource.action_separator}#{action}.:format", action_options)
+          end
+        end
+      end
+    end
+
     def map_member_actions(map, resource)
       resource.member_methods.each do |method, actions|
         actions.each do |action|
           action_options = action_options_for(action, resource, method)
-          
-          if action == :edit
-            map.named_route("#{action}_#{resource.name_prefix}#{resource.singular}", "#{resource.member_path}#{resource.action_separator}#{resource.edit_as}", action_options)
-            map.named_route("formatted_#{action}_#{resource.name_prefix}#{resource.singular}", "#{resource.member_path}#{resource.action_separator}#{resource.edit_as}.:format",action_options)
+
+          if resource.actions_as.has_key?(action)
+            map.named_route("#{action}_#{resource.name_prefix}#{resource.singular}", "#{resource.member_path}#{resource.action_separator}#{resource.actions_as[action]}", action_options)
+            map.named_route("formatted_#{action}_#{resource.name_prefix}#{resource.singular}", "#{resource.member_path}#{resource.action_separator}#{resource.actions_as[action]}.:format", action_options)
           elsif
             map.named_route("#{action}_#{resource.name_prefix}#{resource.singular}", "#{resource.member_path}#{resource.action_separator}#{action}", action_options)
-            map.named_route("formatted_#{action}_#{resource.name_prefix}#{resource.singular}", "#{resource.member_path}#{resource.action_separator}#{action}.:format",action_options)
+            map.named_route("formatted_#{action}_#{resource.name_prefix}#{resource.singular}", "#{resource.member_path}#{resource.action_separator}#{action}.:format", action_options)
           end
         end
       end
@@ -55,6 +101,5 @@ module ActionController
       map.connect(resource.member_path, destroy_action_options)
       map.connect("#{resource.member_path}.:format", destroy_action_options)
     end
-    
   end
 end
