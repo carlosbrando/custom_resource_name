eea1cb7bcb2b3fbeea8973e392d17616827b3f11
carlosbrando <carlosbrando@04ac611f-ab46-0410-9913-d8964a4b2d5a>
2008-03-17 11:49:14 +0000

    Added namespaces alias.
    git-svn-id: http://plugins.nomedojogo.com/custom_resource_name@8 04ac611f-ab46-0410-9913-d8964a4b2d5a

--- lib/custom_resource_name.rb
+++ lib/custom_resource_name.rb
@@ -1,15 +1,16 @@
 module ActionController
   module Resources
     class Resource
-      attr_reader :path_segment, :actions_as, :resources_as
+      attr_reader :path_segment, :actions_as, :resources_as, :namespaces_as
 
       def initialize(entities, options)
         @plural   ||= entities
         @singular ||= options[:singular] || plural.to_s.singularize
 
-        @resources_as = options.delete(:resources_as) || {}
-        @actions_as = options.delete(:actions_as) || {}
-
+        @resources_as  = options.delete(:resources_as) || {}
+        @actions_as    = options.delete(:actions_as) || {}
+        @namespaces_as = options.delete(:namespaces_as) || {}
+        
         @path_segment = options.delete(:as) || @resources_as[entities] || @plural
 
         @options = options
@@ -18,7 +19,7 @@ module ActionController
         add_default_actions
         set_prefixes
       end
-
+      
       def path
         @path ||= "#{path_prefix}/#{path_segment}"
       end
@@ -27,12 +28,21 @@ module ActionController
         action_new = @actions_as[:new] || 'new'
         @new_path ||= "#{path}/#{action_new}"
       end
+      
+      def set_prefixes
+        @path_prefix = change_namespace(options.delete(:path_prefix))
+        @name_prefix = options.delete(:name_prefix)
+      end
+      
+      def change_namespace(prefix)
+        @namespaces_as[prefix].to_sym if prefix && @namespaces_as.has_key?(prefix)
+      end
     end
 
     def map_resource(entities, options = {}, &block)
-      options.merge!(:actions_as => @actions_as, :resources_as => @resources_as)
+      options.merge!(:actions_as => @actions_as, :resources_as => @resources_as, :namespaces_as => @namespaces_as)
       resource = Resource.new(entities, options)
-
+      
       with_options :controller => resource.controller do |map|
         map_collection_actions(map, resource)
         map_default_collection_actions(map, resource)
@@ -46,14 +56,17 @@ module ActionController
         end
       end
     end
-
+    
     def aliases(*args)
       if args.size > 0
         options = args.extract_options!
+        
         if args[0] == :resources
           @resources_as = options || {}
         elsif args[0] == :actions
           @actions_as = options || {}
+        elsif args[0] == :namespaces
+          @namespaces_as = options || {}
         end
       end
     end
@@ -62,7 +75,7 @@ module ActionController
       resource.collection_methods.each do |method, actions|
         actions.each do |action|
           action_options = action_options_for(action, resource, method)
-
+          
           if resource.actions_as.has_key?(action)
             map.named_route("#{action}_#{resource.name_prefix}#{resource.plural}", "#{resource.path}#{resource.action_separator}#{resource.actions_as[action]}", action_options)
             map.named_route("formatted_#{action}_#{resource.name_prefix}#{resource.plural}", "#{resource.path}#{resource.action_separator}#{resource.actions_as[action]}.:format", action_options)
@@ -101,6 +114,5 @@ module ActionController
       map.connect(resource.member_path, destroy_action_options)
       map.connect("#{resource.member_path}.:format", destroy_action_options)
     end
-    
   end
 end
